cf92763bbfe346df454a469f8b73110e2a0b97aa
Saijeet Upadhyay <saijeet2019@gmail.com>
2024-06-25 21:06:27 +0545

    Complete : tab complete

--- script.js
+++ script.js
@@ -49,6 +49,26 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   }
 });
 
+// Tabbed component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+  console.log(clicked);
+  if (!clicked) return;
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  clicked.classList.add("operations__tab--active");
+
+  // Activate content area
+  console.log(clicked.dataset.tab);
+  tabsContent.forEach((t) => t.classList.remove("operations__content--active"));
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
 // /////////////////////////////////
 // const btnScrollTo = document.querySelector(".btn--scroll-to");
 // const section1 = document.querySelector("#section--1");
